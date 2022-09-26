aa3868a9e645c79d4a872024057427d23c045197
vinnieoh <viniciusoliveira926@gmail.com>
2022-09-25 21:01:41 -0300

    Add Docker Configs

--- config/configs.py
+++ config/configs.py
@@ -4,7 +4,7 @@ from sqlalchemy.ext.declarative import declarative_base
 
 class Settings(BaseSettings):
     API_V1_STR: str = '/api/v1'
-    DB_URL: str = 'postgresql+asyncpg://postgres:root12345@localhost:5432/hello-world-fatsapi'
+    DB_URL: str = 'postgresql+asyncpg://root:root12345@db:5432/hello-world-fastapi'
     DBBaseModel = declarative_base()
 
     # Create new token in token_create.py
